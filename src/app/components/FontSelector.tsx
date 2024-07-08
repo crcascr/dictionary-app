6e4e62b0652440365426ca8f1e14875a612564cb
crcascr <cristianj_20@hotmail.com>
2024-07-08 15:40:17 -0500

    Creacion componente FontSelector

---
+++ src/app/components/FontSelector.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setFont } from "../store/fontSlice";
+import { RootState } from "../store";
+
+const FontSelector: React.FC = () => {
+  const dispatch = useDispatch();
+  const currentFont = useSelector((state: RootState) => state.font.currentFont);
+
+  const fonts = ["serif", "sans-serif", "monospace"];
+
+  const handleFontChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(setFont(e.target.value));
+  };
+
+  return (
+    <select
+      value={currentFont}
+      onChange={handleFontChange}
+      className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+    >
+      {fonts.map((font) => (
+        <option key={font} value={font}>
+          {font}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default FontSelector;
