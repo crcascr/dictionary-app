a34a0c90b519a1776f9c8866e8714c562ee0d06d
crcascr <cristianj_20@hotmail.com>
2024-07-08 16:32:02 -0500

    Actualizacion configuracion Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,22 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        serif: ["Georgia", "serif"],
+        sans: ["Arial", "sans-serif"],
+        mono: ["Courier New", "monospace"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    variants: {
+      extend: {},
     },
   },
   plugins: [],
