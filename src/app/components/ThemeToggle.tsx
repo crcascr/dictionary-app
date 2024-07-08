89aab7ef976d0587a7dc8e990a7292a84862bb17
crcascr <cristianj_20@hotmail.com>
2024-07-08 15:34:17 -0500

    Completado componente themeToggle

---
+++ src/app/components/ThemeToggle.tsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "../store/themeSlice";
+import { RootState } from "../store";
+import React from "react";
+
+const ThemeToggle: React.FC = () => {
+  const dispatch = useDispatch();
+  const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode);
+
+  const handleToggle = () => {
+    dispatch(toggleTheme());
+  };
+
+  return (
+    <button
+      onClick={handleToggle}
+      className="p-2 rounded-full bg-gray-200 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    >
+      {isDarkMode ? "☀️" : "🌙"}
+    </button>
+  );
+};
+
+export default ThemeToggle;
